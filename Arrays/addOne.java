3e4255bab397603499a4258b5cdaf6e305d448c8
bhasin11 <bhasinsidharth97@gmail.com>
2016-12-21 17:22:38 -0800

    Update addOne.java

--- Arrays/addOne.java
+++ Arrays/addOne.java
@@ -5,29 +5,29 @@ Add 1 to the number.
 */
 
 class PracticeArray{
-	public int[] plusOne(int[] digits) {
-		
-		if(digits == null) return null;
-		
-		int lastNotNineIndex=-1;
-		
-		for(int i=0;i<digits.length;i++)
-			if(digits[i] != 9) lastNotNineIndex = i;
+    public int[] plusOne(int[] digits) {
 
-		if(lastNotNineIndex != -1) 
-			digits[lastNotNineIndex] = digits[lastNotNineIndex] + 1;
-		
-		for(int i = lastNotNineIndex+1;i<digits.length;i++)
-			digits[i]=0;
-		
-		if(lastNotNineIndex == -1){
-			int[] newDigits = new int[digits.length+1];
-			newDigits[0] = 1;
-			for(int i=1;i<digits.length;i++)
-				newDigits[i] = digits[i-1];
+	if(digits == null) return null;
 
-			return newDigits;
-		}
-		return digits;
+	int lastNotNineIndex=-1;
+
+	for(int i=0;i<digits.length;i++)
+		if(digits[i] != 9) lastNotNineIndex = i;
+
+	if(lastNotNineIndex != -1) 
+		digits[lastNotNineIndex] = digits[lastNotNineIndex] + 1;
+
+	for(int i = lastNotNineIndex+1;i<digits.length;i++)
+		digits[i]=0;
+
+	if(lastNotNineIndex == -1){
+		int[] newDigits = new int[digits.length+1];
+		newDigits[0] = 1;
+		for(int i=1;i<digits.length;i++)
+			newDigits[i] = digits[i-1];
+
+		return newDigits;
+	}
+	return digits;
     }
 }
